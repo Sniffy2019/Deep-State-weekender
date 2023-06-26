50ad6f226a6313bd813a62160e8cddefa4360385
Alexander Parry <122370573+Sniffy2019@users.noreply.github.com>
2023-06-26 13:42:16 +0100

    Update map.js

--- assets/map.js
+++ assets/map.js
@@ -4,10 +4,19 @@ async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
 
   map = new Map(document.getElementById("map"), {
-    center: { lat: 51.502949, lng: 0.002369 },
+    center: { lat: 51.5029, lng: 0.0032 },
     zoom: 15,
   });
 }
 
-
 initMap();
+
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
